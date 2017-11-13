0d5366c7619dd8ccec673104090cebaf073269ee
marcusfgardiner <marcusfgardiner@gmail.com>
2017-11-13 18:28:57 +0000

    Second commit, creating temp converter

---
+++ chapter1_celsius_converter/c2f.rb
@@ -0,0 +1,7 @@
+#Tempeature Converter file
+
+celsius = 100
+fahrenheit = (celsius*9 / 5)/ + 32
+print "The result is: "
+print fahrenheit
+puts "."
---
+++ chapter1_celsius_converter/c2fi.rb
@@ -0,0 +1,4 @@
+#Interactive Tempeature Converter file
+
+puts "Hello, please enter a celsius value"
+print "The result is ", gets.to_i*9 / 5 + 32, ".\n"
