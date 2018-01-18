a108fa3351751a126321d1f19b2a0972735dd4a4
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-18 13:03:00 +0000

    Part 1 complete, doing a review and revision

--- chapter2_objects/args_unleashed_how_ruby_arguments_work.rb
+++ chapter2_objects/args_unleashed_how_ruby_arguments_work.rb
@@ -3,7 +3,10 @@ def args_unleashed(a,b=1,*c,d,e)
   p a,b,c,d,e
 end
 
-# a,d,e take priority as requiring values for method to work, then b=1 does with its default value being overtaken, with nothing left for *c which soaks up the rest
+# a,d,e take priority as requiring values for method to work,
+# then b=1 does with its default value being overtaken,
+# with nothing left for *c which soaks up the rest
+
 #stays in order, skipping c
 args_unleashed(1,2,3,4)
 
@@ -12,3 +15,8 @@ args_unleashed(1,2,3,4,5,6,7,8)
 
 #not enough for b=1 or *c, a,d,e take priority
 args_unleashed(1,2,3)
+
+
+# #the most important thing to take away from these examples is the perhaps obvious point that,
+# no matter what you do, every parameter ends up bound to some value.
+# even if its just bound to an empty array like *c can be when there are no arguments for it to sponge
